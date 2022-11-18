8db1146b17f5c90a6daf6859802cb665731eaee7
asheikh <asim.sardar.sheikh@gmail.com>
2022-11-18 14:47:54 +1100

    removing example.ts and creating guestbook route

---
+++ src/server/trpc/router/guestbook.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+import { router, publicProcedure } from "../trpc";
+
+export const guestbookRouter = router({
+  postMessage: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        message: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        await ctx.prisma.guestBook.create({
+          data: {
+            name: input.name,
+            message: input.message,
+          },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }),
+});
