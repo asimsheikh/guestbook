02572011995c61beb423fd25d260a1f7143d40a0
asheikh <asim.sardar.sheikh@gmail.com>
2022-12-05 13:23:17 +1100

    static rendering of tasks from db.json

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,17 +1,33 @@
+import fs from "fs/promises";
+import path from "path";
+
 import { type NextPage } from "next";
 
-const Home: NextPage = () => {
-  const { data: session, status } = useSession();
-
-  if (status == "loading") {
-    return <main className="flex flex-col items-center pt-4">Loading...</main>;
-  }
-
+const Home: NextPage = (props) => {
   return (
     <main className="m-2 flex flex-col items-center">
-      <div>Tasks Page</div>
+      <div>{JSON.stringify(props.data.tasks)}</div>
+      <div>
+        <h1 className="text-2xl font-extrabold">Tasks Page</h1>
+      </div>
+      <div className="my-2"></div>
+      <ul>
+        {props.data.tasks.map((task) => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
     </main>
   );
 };
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "db.json");
+  const data = await fs.readFile(filePath);
+  const jsonData = JSON.parse(data);
+
+  return {
+    props: { data: jsonData },
+  };
+}
 
 export default Home;
