db149ced2993c4ffc93a91d9445aca855ecf08c1
asheikh <asim.sardar.sheikh@gmail.com>
2022-12-06 13:47:19 +1100

    Adding getServerSideProprs to the user-profile page

--- src/pages/user-profile.tsx
+++ src/pages/user-profile.tsx
@@ -1,7 +1,18 @@
 import { type NextPage } from "next";
 
-const UserProfile = () => {
-  return <div>User Profile Page</div>;
+const UserProfile = (props) => {
+  return (
+    <div>
+      <h1>Hello, {props.username}</h1>
+    </div>
+  );
 };
 
+export async function getServerSideProps(context) {
+  console.log(Object.keys(context));
+  console.log(Object.keys(context.req));
+  return {
+    props: { username: "Asim Sheikh" },
+  };
+}
 export default UserProfile;
